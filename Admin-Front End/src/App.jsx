77ea86f9d379efd155a9f6859576d014e646dbc5
Mohamed Raashid <raashidm862@gmail.com>
2024-10-14 15:53:19 +0530

    Refactor API endpoint for accepting events in DisplayEvent component Refactor route paths and components in App.jsx and AddEvents.jsx Remove unnecessary files and configurations Add license information to axios package-lock.json Update AddPlaces component to display requested new places Add RequestedPlaces component to display and delete requested places Add CSS styles for RequestedPlaces component

--- Admin-Front End/src/App.jsx
+++ Admin-Front End/src/App.jsx
@@ -12,10 +12,10 @@ import Root from "./pages/ForgotPwd/Root";
 import Homepage1 from "./pages/ForgotPwd/Homepage1";
 import Homepage2 from "./pages/SignIn/Homepage2";
 import Body from "./pages/HomePage/Body/Body";
-import AddPlaceForm from './pages/HomePage/Place/AddPlaceForm/AddPlaceForm';
+import AddPlaceForm from "./pages/HomePage/Place/AddPlaceForm/AddPlaceForm";
 import DataDisplay from "./pages/DisplayEvent/DisplayEvent";
 import AcceptedEvent from "./pages/DisplayEvent/AcceptedEvent/AcceptedEvent";
-
+import RequestedPlaces from "./pages/HomePage/Place/RequstedPlaces/RequestedPlaces";
 
 function App() {
   const action = useNavigationType();
@@ -68,12 +68,15 @@ function App() {
       case "/22":
         title = "";
         metaDescription = "";
-        break
+        break;
       case "/23":
         title = "";
         metaDescription = "";
         break;
-        
+      case "/24":
+        title = "";
+        metaDescription = "";
+        break;
     }
 
     if (title) {
@@ -90,11 +93,9 @@ function App() {
     }
   }, [pathname]);
 
-  
-
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />  
+      <Route path="/" element={<HomePage />} />
       <Route path="/19" element={<Shop />} />
       <Route path="/18" element={<Root1 />} />
       <Route path="/17" element={<Root />} />
@@ -102,22 +103,19 @@ function App() {
       <Route path="/12" element={<Homepage1 />} />
       <Route path="/16" element={<Homepage2 />} />
       <Route path="/20" element={<Body />} />
-      <Route path="/21" element={<AddPlaceForm />} />
-      <Route path="/22" element={<DataDisplay />} /> 
+      <Route path="/21" element={<RequestedPlaces />} />
+      <Route path="/22" element={<DataDisplay />} />
       <Route path="/23" element={<AcceptedEvent />} />
-
-
+      <Route path="/24" element={<AddPlaceForm />} />
     </Routes>
   );
-
 }
 export default App;
- /* / is for first page (Sign up) */
- /* /19 is for last page (reset password) */
- /* /18 is for OTP typing page (Sign up) */
- /* /17 is for Check your mail popup page */
- /* /12 is for forget password (3rd page) */
- /* /16 is for Sign in page */
- /*/21 is for DisplayEvent page */
-  /*/23 is for AcceptedEvent page */
- 
+/* / is for first page (Sign up) */
+/* /19 is for last page (reset password) */
+/* /18 is for OTP typing page (Sign up) */
+/* /17 is for Check your mail popup page */
+/* /12 is for forget password (3rd page) */
+/* /16 is for Sign in page */
+/*/21 is for DisplayEvent page */
+/*/23 is for AcceptedEvent page */
